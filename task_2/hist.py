61c29db8a7136e14d1c75be1deae25f500bda55f
eliakimceleste <eliakimceleste@gmail.com>
2024-06-27 14:56:40 +0100

    bref modification

--- task_2/hist.py
+++ task_2/hist.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #Importer le dataset
 
-df=pd.read_csv('task_2/Housing.csv')
+df=pd.read_csv('./Housing.csv')
 
 #Voir la distribution pour la variable bedrooms avec matplotlib
 plt.hist(df['bedrooms'],bins=10,edgecolor='black')
